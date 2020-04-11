b66305d78cc5d389189fa7ccce26efcd430e698e
Max <maxim.ananin@bravedevelopers.com>
2020-04-11 19:53:45 +0400

    added DetailFragment

--- app/src/main/java/com/bravemax/animals/view/fragments/DetailFragment.kt
+++ app/src/main/java/com/bravemax/animals/view/fragments/DetailFragment.kt
@@ -1,4 +1,31 @@
 package com.bravemax.animals.view.fragments
 
-class DetailFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.bravemax.animals.R
+import com.bravemax.animals.databinding.FragmentDetailBinding
+import com.bravemax.animals.model.Animal
+
+class DetailFragment : Fragment() {
+
+    private var animalArg: Animal? = null
+    private lateinit var dataBinding: FragmentDetailBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        dataBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_detail, container, false)
+        return dataBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
 }
